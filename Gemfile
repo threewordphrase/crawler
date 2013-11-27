3fba48ade7548e4bf96e49323788fe124d600c7e
Aaron Smith <aaron.smith@commonmediainc.com>
2013-11-27 15:02:38 -0500

    inital commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'anemone'
+
+gem 'nokogiri'
+
+gem 'mechanize'
+
+gem 'wombat'
---
+++ crawl.rb
@@ -0,0 +1,43 @@
+# require 'mechanize'
+# require 'rubygems'
+# require 'wombat'
+
+
+###########################
+# drush @lumicorelocal uli
+
+require 'anemone'
+require 'nokogiri'
+require 'json'
+require 'csv'
+
+domains = JSON.parse(File.read('./domains.json'));
+
+results = []
+
+domains.each do |domain|
+
+	Anemone.crawl(domain) do |anemone|
+
+	  anemone.on_every_page do |page|
+	  	# puts "############################"
+	      # puts page.response_time.to_s+'|'+page.code.to_s+'|'+page.url.to_s
+	      lipsum = !!(page.body =~ /Lorem|ipsum|dolor|amet/)
+
+	      puts page.url.to_s
+
+	      row = [page.response_time, page.code, page.url.to_s, lipsum]
+
+	      results << row 
+	      
+	  end
+
+	end
+
+end
+
+CSV.open("./output.csv", "wb") do |csv|
+	results.each do |r|
+		csv << r
+	end
+end
